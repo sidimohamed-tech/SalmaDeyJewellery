024fc05a165366d5e79362762ab9e38f6d877450
Sidi <ton.email@exemple.com>
2026-01-31 02:03:24 +0100

    remove videos of Homepage an their title

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -151,7 +151,7 @@ const Index = () => {
         <section className="py-16">
           <div className="container mx-auto px-4 lg:px-8">
             <h2 className="text-3xl lg:text-4xl font-serif text-center mb-8">
-              Our Packaging and Advertisements
+              Our Packaging
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
               {/* Packaging Images */}
